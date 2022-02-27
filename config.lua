60012f8dbb25e82f61aaff964be2539d8e772d5f
JackTheDevFMD <99100073+JackTheDevFMD@users.noreply.github.com>
2022-02-27 21:10:29 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,23 @@
+config = {}
+
+config.MarkerSize = {x = 1.5, y = 1.5, z = 0.5}
+config.MarkerColor = {r = 50, g = 50, b = 204}
+
+config.locations = {
+    LSPD = {
+        Blip = {
+			Coords  = vector3(425.1, -979.5, 30.7),
+			Sprite  = 60,
+			Display = 4,
+			Scale   = 1.2,
+			Colour  = 29
+		},
+		blipCoords = {
+			vector3(458.49, -998.61, 29.7), -- Misson Row
+			vector3(362.98, -1593.86, 25.45), -- Davis
+			vector3(-1085.84, -831.17, 15.64), -- Vespucci
+			vector3(844.84, -1286.64, 24.32), -- La Mesa
+			vector3(-438.2, 6012.3, 37) -- Paleto
+		}
+    },
+}
